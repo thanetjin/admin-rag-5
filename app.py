517d93e7c467e8caca5af312e9bb78539c7bb3a5
thanetjin <108661996+thanetjin@users.noreply.github.com>
2025-03-27 02:07:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -383,19 +383,20 @@ def create_english():
 def create_gen():
     if "email" in session:
         if request.method == "POST":                                
-            credentials_json = {
-            "type": os.getenv("TYPE"),
-            "project_id": os.getenv("PROJECT_ID"),
-            "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-            "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),
-            "client_email": os.getenv("CLIENT_EMAIL"),
-            "client_id": os.getenv("CLIENT_ID"),
-            "auth_uri": os.getenv("AUTH_URI"),
-            "token_uri": os.getenv("TOKEN_URI"),
-            "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
-            "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
-            "universe_domain": os.getenv("UNIVERSE_DOMAIN")
-        }
+            private_key = os.getenv("PRIVATE_KEY").replace('\\n', '\n')
+            credentials = {
+                "type": os.getenv("TYPE"),
+                "project_id": os.getenv("PROJECT_ID"),
+                "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+                "private_key": private_key,
+                "client_email": os.getenv("CLIENT_EMAIL"),
+                "client_id": os.getenv("CLIENT_ID"),
+                "auth_uri": os.getenv("AUTH_URI"),
+                "token_uri": os.getenv("TOKEN_URI"),
+                "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+                "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+                "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
+            }
             scopes = [
                 "https://www.googleapis.com/auth/spreadsheets"
             ]            
@@ -520,19 +521,20 @@ def create_policy():
     if "email" in session:
         if request.method == "POST":                    
             
-            credentials_json = {
-            "type": os.getenv("TYPE"),
-            "project_id": os.getenv("PROJECT_ID"),
-            "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-            "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),
-            "client_email": os.getenv("CLIENT_EMAIL"),
-            "client_id": os.getenv("CLIENT_ID"),
-            "auth_uri": os.getenv("AUTH_URI"),
-            "token_uri": os.getenv("TOKEN_URI"),
-            "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
-            "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
-            "universe_domain": os.getenv("UNIVERSE_DOMAIN")
-        }
+            private_key = os.getenv("PRIVATE_KEY").replace('\\n', '\n')
+            credentials = {
+                "type": os.getenv("TYPE"),
+                "project_id": os.getenv("PROJECT_ID"),
+                "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+                "private_key": private_key,
+                "client_email": os.getenv("CLIENT_EMAIL"),
+                "client_id": os.getenv("CLIENT_ID"),
+                "auth_uri": os.getenv("AUTH_URI"),
+                "token_uri": os.getenv("TOKEN_URI"),
+                "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+                "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+                "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
+            }
             scopes = [
                 "https://www.googleapis.com/auth/spreadsheets"
             ]            
